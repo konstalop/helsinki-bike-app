785d7fe3aeae206a0ea1e3c494428b463aabb90a
Konsta <konlopponen@gmail.com>
2022-08-01 22:14:05 +0300

    create server, add needed node packages

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => 
+console.log('Connected to MongoDB')
+).catch(err => console.error(err))
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`))
