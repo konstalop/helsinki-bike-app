23d98c1c5c95f3b17a966e25c4bb5958f94eb8c5
Konsta <konlopponen@gmail.com>
2022-08-08 16:02:00 +0300

    fix typo in viewsinglestation

--- client/src/components/stations/ViewSingleStation.js
+++ client/src/components/stations/ViewSingleStation.js
@@ -5,7 +5,7 @@ const ViewSingleStation = ({setTrigger, station}) => {
     return (
     <div className='single-station-container'>
         <div className='single-station-inner'>
-        <h2 className='single-station-h2'>Vieving station {station.nameFi}</h2>
+        <h2 className='single-station-h2'>Viewing station {station.nameFi}</h2>
         <SingleStationTable station={station}/>
         <button
             className='done-btn'
