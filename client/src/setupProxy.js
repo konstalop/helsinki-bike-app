09b13885f81bc6e3de79cc14718b5354adba3515
Konsta <konlopponen@gmail.com>
2022-08-09 19:02:39 +0300

    comment code

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,9 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
+/**
+ * Setupping proxy which enables communication between backend and frontend
+ * in the same URL.
+ * @param {*} app 
+ */
 module.exports = function(app) {
   app.use(
     '/api',
