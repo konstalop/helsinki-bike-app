d278a790c50ca363ec07cf35ba503bf06c4bd2fb
Konsta <konlopponen@gmail.com>
2022-08-11 17:19:57 +0300

    fix server

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use('/api/stations', stationRouter)
 app.use('/api/stats', statsRouter)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
+    app.use(express.static('../client/build'))
 
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
